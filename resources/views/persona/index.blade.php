5310556e49aded495cfae9a740ee6c0752f314f1
dev-resdark <sresquin@outlook.com>
2022-10-26 18:44:49 -0300

    Cambio realizado a la vista index.blade.php

--- resources/views/persona/index.blade.php
+++ resources/views/persona/index.blade.php
@@ -35,46 +35,11 @@
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
-                                        
-										<th>Antecedentepolicial</th>
-										<th>Fechaemision</th>
-										<th>Fechavencimiento</th>
-										<th>Nrocedula</th>
-										<th>Nroruc</th>
-										<th>Digitoindicador</th>
-										<th>Primerapellido</th>
-										<th>Segundoapellido</th>
-										<th>Apellidocasada</th>
-										<th>Primernombre</th>
-										<th>Segundonombre</th>
-										<th>Sexo</th>
-										<th>Fechanaciemiento</th>
-										<th>Paisnacimiento</th>
-										<th>Ciudadnacimiento</th>
-										<th>Paisresidenciaactual</th>
-										<th>Estadocivil</th>
-										<th>Cantidadhijosedades</th>
-										<th>Telefono</th>
-										<th>Celular</th>
-										<th>Correo</th>
-										<th>Calleprincipal</th>
-										<th>Calletransversal</th>
-										<th>Nrocasa</th>
-										<th>Departamento</th>
-										<th>Ciudad</th>
-										<th>Barrio</th>
-										<th>Cargounibe</th>
-										<th>Areadesempeño</th>
-										<th>Lugardesempeñoasuncionsanlorenzo</th>
-										<th>Gradoacademico</th>
-										<th>Subirdocumentoidentidad</th>
-										<th>Subircv</th>
-										<th>Adjtitulosgrado</th>
-										<th>Adjtitulopostgrado</th>
-										<th>Adjotroscertificados</th>
-										<th>Adjregistroprofesional</th>
-										<th>Enfermedades</th>
-										<th>Contactoemergencia</th>
+
+										<th>Nro Cedula</th>
+										<th>Primer Apellido</th>
+										<th>Primer Nombre</th>
+										<th>Fecha naciemiento</th>
 
                                         <th></th>
                                     </tr>
@@ -83,46 +48,12 @@
                                     @foreach ($personas as $persona)
                                         <tr>
                                             <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $persona->antecedentePolicial }}</td>
-											<td>{{ $persona->fechaEmision }}</td>
-											<td>{{ $persona->fechaVencimiento }}</td>
 											<td>{{ $persona->nroCedula }}</td>
-											<td>{{ $persona->nroRuc }}</td>
-											<td>{{ $persona->digitoIndicador }}</td>
 											<td>{{ $persona->primerApellido }}</td>
-											<td>{{ $persona->segundoApellido }}</td>
-											<td>{{ $persona->apellidoCasada }}</td>
 											<td>{{ $persona->primerNombre }}</td>
-											<td>{{ $persona->segundoNombre }}</td>
-											<td>{{ $persona->sexo }}</td>
 											<td>{{ $persona->fechaNaciemiento }}</td>
-											<td>{{ $persona->paisNacimiento }}</td>
-											<td>{{ $persona->ciudadNacimiento }}</td>
-											<td>{{ $persona->paisResidenciaActual }}</td>
-											<td>{{ $persona->estadoCivil }}</td>
-											<td>{{ $persona->cantidadHijosEdades }}</td>
-											<td>{{ $persona->telefono }}</td>
-											<td>{{ $persona->celular }}</td>
-											<td>{{ $persona->correo }}</td>
-											<td>{{ $persona->callePrincipal }}</td>
-											<td>{{ $persona->calleTransversal }}</td>
-											<td>{{ $persona->nroCasa }}</td>
-											<td>{{ $persona->departamento }}</td>
-											<td>{{ $persona->ciudad }}</td>
-											<td>{{ $persona->barrio }}</td>
-											<td>{{ $persona->cargoUnibe }}</td>
-											<td>{{ $persona->areaDesempeño }}</td>
-											<td>{{ $persona->lugarDesempeñoAsuncionSanLorenzo }}</td>
-											<td>{{ $persona->gradoAcademico }}</td>
-											<td>{{ $persona->subirDocumentoIdentidad }}</td>
-											<td>{{ $persona->subirCv }}</td>
-											<td>{{ $persona->adjTitulosGrado }}</td>
-											<td>{{ $persona->adjTituloPostgrado }}</td>
-											<td>{{ $persona->adjOtrosCertificados }}</td>
-											<td>{{ $persona->adjRegistroProfesional }}</td>
-											<td>{{ $persona->enfermedades }}</td>
-											<td>{{ $persona->contactoEmergencia }}</td>
+                                            
+											
 
                                             <td>
                                                 <form action="{{ route('personas.destroy',$persona->id) }}" method="POST">
